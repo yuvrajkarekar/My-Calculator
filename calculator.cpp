cbebe89a914aaba766143fdaaa4a267a651a7ca2
Vipul Bagde <vipulbagde1897@gmail.com>
2025-09-02 16:48:57 +0530

    unknown

--- calculator.cpp
+++ calculator.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int choice;
     int res, a, b;
-    cout<<"1. Add\n2. Sub";
+    cout<<"1. Add\n2. Sub\n3. Div\n ";
     cin>>choice;
     cout<<"Enter Two Numbers : ";
     cin>> a  >> b ;
@@ -21,6 +21,9 @@ int main()
         res = a - b;
         cout<<"Res = "<< res;
         break;
+    case 3:
+        res=a / b;
+        cout<<"Res = "<< res;
     default:
         break;
     }
