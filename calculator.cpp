11d2df170840cf3d76959717daf64bbaeabee54d
Yuvraj Karekar <yuvrajkarekar5@gamil.com>
2025-09-02 15:27:19 +0530

    First commit

---
+++ calculator.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int choice;
+    int res, a, b;
+    cout<<"1. Add\n2. Sub";
+    cin>>choice;
+    cout<<"Enter Two Numbers : ";
+    cin>> a  >> b ;
+
+    switch (choice)
+    {
+    case 1:
+        res = a + b;
+        cout<<"Res = "<< res;
+        break;
+    case 2:
+        res = a - b;
+        cout<<"Res = "<< res;
+        break;
+    default:
+        break;
+    }
+
+
+    return 0;
+}
